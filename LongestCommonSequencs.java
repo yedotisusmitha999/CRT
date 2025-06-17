72136d0b43f9d869712644d4cb8bd60cf71cf64d
Yedoti Susmitha <168511579+yedotisusmitha999@users.noreply.github.com>
2025-06-17 11:06:49 +0530

    Create LongestCommonSequence.java

---
+++ LongestCommonSequencs.java
@@ -0,0 +1,25 @@
+
+public class LongestCommonSequence {
+    public static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+        System.out.println("Length of LCS: " + lcs(s1, s2)); // Output: 4
+    }
+}
