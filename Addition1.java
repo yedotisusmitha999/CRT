9eb433176933e65208b495bc075d53ec1c3adc4b
Yedoti Susmitha <168511579+yedotisusmitha999@users.noreply.github.com>
2025-06-17 11:26:39 +0530

    Create Addition1.java

---
+++ Addition1.java
@@ -0,0 +1,10 @@
+
+import java.io.*;
+//import java.util.*;
+public class addition1{
+    public static void main(String[] args){
+        int a=10;
+        double b=3.5;
+        System.out.println(a+b);
+    }
+}
