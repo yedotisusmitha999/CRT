f311986fe3e179573289e5ac4d77de081ee9a354
Yedoti Susmitha <168511579+yedotisusmitha999@users.noreply.github.com>
2025-06-17 11:10:31 +0530

    Create OptimizedBubblesort.java

---
+++ OptimizedBubblesort.java
@@ -0,0 +1,29 @@
+
+public class OptimizedBubbleSort {
+    public static void optimizedBubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        optimizedBubbleSort(arr);
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
